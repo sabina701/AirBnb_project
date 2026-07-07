5345a0c4785b53689d055be57427618285da925b
sabina701 <shresthasabeena9@gmail.com>
2026-07-07 19:19:18 +0545

    Integrate Multer and Cloudinary for image uploads

--- routes/listing.js
+++ routes/listing.js
@@ -6,12 +6,16 @@ const Listing = require("../models/listings");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 //controllers
 const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 router
   .route("/")
   .get(wrapAsync(listingController.index))
   .post(
     isLoggedIn,
+    upload.single("listing[image]"),
     validateListing,
     wrapAsync(listingController.createListing),
   );
