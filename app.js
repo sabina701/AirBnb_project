268728d737208fc6fe2c473f59b666c97c4206ef
sabina701 <shresthasabeena9@gmail.com>
2026-08-03 11:51:39 +0545

    database changed

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLASDB_URL;
 main()
   .then(() => console.log(`connection successful`))
   .catch((err) => console.log(err));
